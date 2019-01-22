427d26d6ee431210446e31d7f3bce13a2023bf50
Carolin Hollweger <carolinhollweger@carolins-mbp.edu.htl.org>
2019-01-22 10:17:48 +0100

    juhu

---
+++ Assets/Scripts/Parallaxer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallaxer : MonoBehaviour
+{
+
+    class PoolObject
+    {
+        public Transform transform;
+        public bool inUse;
+        public PoolObject(Transform t) { transform = t; }
+        public void Use() { inUse = true; }
+        public void Dispose() { inUse = false; }
+    }
+
+    [System.Serializable]
+    public struct YSpawnRange
+    {
+        public float min;
+        public float max;
+    }
+}
---
+++ Assets/Scripts/StopWatch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StopWatch : MonoBehaviour
+{
+
+    private Text watch;
+    private float timer;
+
+    // Use this for initialization
+    void Start()
+    {
+        watch = gameObject.GetComponent<Text>();
+        watch.text = "00:00:00";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        watch.text = string.Format("{0:00}:{1:00}.{2:00}",
+            Mathf.Floor(timer / 60),
+            Mathf.Floor(timer) % 60,
+            Mathf.Floor((timer * 100) % 100));
+    }
+}
